4ff247706bda34475503323a5aee1abcd72262e1
PaulAbloescher <abloescherpaul@gmail.com>
2021-05-04 17:03:23 +0200

    Add helper class for next and previous rank calculation.

---
+++ src/Wator.MPI/Communication/TargetRankHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wator.MPI.Communication
+{
+    public class TargetRankHelper
+    {
+        public static int GetNextProcessRank(int currentRank, int worldSize)
+        {
+            return (currentRank % worldSize) + 1;
+        }
+
+        public static int GetPreviousProcessRank(int currentRank, int worldSize)
+        {
+            return (currentRank + 1) % worldSize + 1;
+        }
+    }
+}
---
+++ test/Wator.MPI.Tests/TargetRankHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Wator.MPI.Communication;
+
+namespace Wator.MPI.Tests
+{
+    public class TargetRankHelperTests
+    {
+        [Theory]
+        [InlineData(1, 3, 3)]
+        [InlineData(2, 3, 1)]
+        [InlineData(3, 3, 2)]
+        public void Can_Calculate_Previous_Rank_From_Current_Rank(int currentRank, int worldSize, int expectedTargetRank)
+        {
+            var actual = TargetRankHelper.GetPreviousProcessRank(currentRank, worldSize);
+            Assert.Equal(actual, expectedTargetRank);
+        }
+
+        [Theory]
+        [InlineData(1, 3, 2)]
+        [InlineData(2, 3, 3)]
+        [InlineData(3, 3, 1)]
+        public void Can_Calculate_Next_Rank_From_Current_Rank(int currentRank, int worldSize, int expectedTargetRank)
+        {
+            var actual = TargetRankHelper.GetNextProcessRank(currentRank, worldSize);
+            Assert.Equal(actual, expectedTargetRank);
+        }
+    }
+}
