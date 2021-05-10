85ad2a7e2ff33cba196314cc9e69df685381d184
PaulAbloescher <abloescherpaul@gmail.com>
2021-05-10 20:54:32 +0200

    Implement cycle logic WIP.

--- src/Wator.Core/Services/FieldService.cs
+++ src/Wator.Core/Services/FieldService.cs
@@ -1,10 +1,53 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Wator.Core.Services
 {
     public class FieldService
     {
+        private static readonly Random _random = new();
+
+        public static IEnumerable<(int RowIndex, int ColumnIndex)> RunCycleInRows(int fromRow, int toRow,
+            sbyte[,] field)
+        {
+            for (var rowIndex = 0; rowIndex <= toRow; rowIndex++)
+            for (var colIndex = 0; colIndex < field.GetLength(1); colIndex++)
+            {
+                var oldPosition = (rowIndex, colIndex);
+
+                if (field[oldPosition.rowIndex, oldPosition.colIndex] == 0) continue;
+
+                if (field[oldPosition.rowIndex, oldPosition.colIndex] > 0)
+                {
+                    // Fish move
+                    var newPositionCandidates = GetFishSurroundingFieldCandidates(oldPosition, field);
+                    var newPosition = ChooseField(newPositionCandidates);
+                    MoveToField(oldPosition, newPosition, field);
+                    // Eier legen ;)
+                }
+                else
+                {
+                    // Shark move
+                    var newPositionCandidates = GetSharkSurroundingFieldCandidates(oldPosition, field);
+                }
+            }
+
+            return Enumerable.Empty<(int, int)>();
+        }
+
+        public static void MoveToField((int RowIndex, int ColumnIndex) oldPosition,
+            (int RowIndex, int ColumnIndex) newPosition, sbyte[,] field)
+        {
+            field[newPosition.RowIndex, newPosition.ColumnIndex] = field[oldPosition.RowIndex, oldPosition.ColumnIndex];
+            field[oldPosition.RowIndex, oldPosition.ColumnIndex] = 0;
+        }
+
+        public static (int RowIndex, int ColumnIndex) ChooseField(IEnumerable<(int, int)> candidates)
+        {
+            return candidates.ElementAt(_random.Next(0, candidates.Count()));
+        }
+
         public static IEnumerable<(int RowIndex, int ColumnIndex)> GetSurroundingFields(
             (int RowIndex, int ColumnIndex) position, sbyte[,] field)
         {
@@ -14,7 +57,8 @@ namespace Wator.Core.Services
             yield return (position.RowIndex, (position.ColumnIndex - 1 + field.GetLength(1)) % field.GetLength(1));
         }
 
-        public static IEnumerable<(int RowIndex, int ColumnIndex)> GetSharkSurroundingFieldCandidates((int RowIndex, int ColumnIndex) position, sbyte[,] field)
+        public static IEnumerable<(int RowIndex, int ColumnIndex)> GetSharkSurroundingFieldCandidates(
+            (int RowIndex, int ColumnIndex) position, sbyte[,] field)
         {
             var surroundingFields = GetSurroundingFields(position, field).ToArray();
 
@@ -24,7 +68,8 @@ namespace Wator.Core.Services
             return surroundingFields.Where(pos => IsEmpty(pos, field));
         }
 
-        public static IEnumerable<(int RowIndex, int ColumnIndex)> GetFishSurroundingFieldCandidates((int RowIndex, int ColumnIndex) position, sbyte[,] field)
+        public static IEnumerable<(int RowIndex, int ColumnIndex)> GetFishSurroundingFieldCandidates(
+            (int RowIndex, int ColumnIndex) position, sbyte[,] field)
         {
             var surroundingFields = GetSurroundingFields(position, field).ToArray();
             return surroundingFields.Where(pos => IsEmpty(pos, field));
