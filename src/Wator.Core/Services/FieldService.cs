08880b1d3375854d0c0dbab02f53b1b02e02b937
PaulAbloescher <abloescherpaul@gmail.com>
2021-05-10 20:19:13 +0200

    Implement next field candidates logic.

--- src/Wator.Core/Services/FieldService.cs
+++ src/Wator.Core/Services/FieldService.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Wator.Core.Services
 {
@@ -15,6 +12,32 @@ namespace Wator.Core.Services
             yield return (position.RowIndex, (position.ColumnIndex + 1) % field.GetLength(1));
             yield return ((position.RowIndex + 1) % field.GetLength(0), position.ColumnIndex);
             yield return (position.RowIndex, (position.ColumnIndex - 1 + field.GetLength(1)) % field.GetLength(1));
+        }
+
+        public static IEnumerable<(int RowIndex, int ColumnIndex)> GetSharkSurroundingFieldCandidates((int RowIndex, int ColumnIndex) position, sbyte[,] field)
+        {
+            var surroundingFields = GetSurroundingFields(position, field).ToArray();
+
+            if (surroundingFields.Any(pos => ContainsFish(pos, field)))
+                return surroundingFields.Where(pos => ContainsFish(pos, field));
+
+            return surroundingFields.Where(pos => IsEmpty(pos, field));
+        }
+
+        public static IEnumerable<(int RowIndex, int ColumnIndex)> GetFishSurroundingFieldCandidates((int RowIndex, int ColumnIndex) position, sbyte[,] field)
+        {
+            var surroundingFields = GetSurroundingFields(position, field).ToArray();
+            return surroundingFields.Where(pos => IsEmpty(pos, field));
+        }
+
+        private static bool ContainsFish((int RowIndex, int ColumnIndex) position, sbyte[,] field)
+        {
+            return field[position.RowIndex, position.ColumnIndex] > 0;
+        }
+
+        private static bool IsEmpty((int RowIndex, int ColumnIndex) position, sbyte[,] field)
+        {
+            return field[position.RowIndex, position.ColumnIndex] == 0;
         }
     }
 }
