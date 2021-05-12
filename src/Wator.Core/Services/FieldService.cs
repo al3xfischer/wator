978809c5befa08114f021bb8501d117c72984574
PaulAbloescher <abloescherpaul@gmail.com>
2021-05-12 16:20:15 +0200

    Extract array access to method.

--- src/Wator.Core/Services/FieldService.cs
+++ src/Wator.Core/Services/FieldService.cs
@@ -15,63 +15,51 @@ namespace Wator.Core.Services
             var moved = new List<Position>();
 
             for (var rowIndex = 0; rowIndex <= toRow; rowIndex++)
-                for (var colIndex = 0; colIndex < field.GetLength(1); colIndex++)
+            for (var colIndex = 0; colIndex < field.GetLength(1); colIndex++)
+            {
+                var oldPosition = new Position(rowIndex, colIndex);
+                var animal = GetAnimalAtPosition(field, oldPosition);
+
+                if (animal is null || moved.Contains(oldPosition)) continue;
+
+                if (animal.Type == AnimalType.Fish)
                 {
-                    var oldPosition = new Position(rowIndex, colIndex);
-                    var animal = field[oldPosition.RowIndex, oldPosition.ColumnIndex];
-
-                    if (animal is null || moved.Contains(oldPosition)) continue;
-
-                    if (animal.Type == AnimalType.Fish)
-                    {
-                        // Fish move
-                        var newPositionCandidates = GetFishSurroundingFieldCandidates(oldPosition, field);
-                        var newPosition = ChooseField(newPositionCandidates);
-                        animal.Age++;
-                        animal.Energy--;
-                        MoveToField(oldPosition, newPosition, field);
-                        moved.Add(newPosition);
-                        if (animal.Age % 3 == 0)
-                        {
-                            field[oldPosition.RowIndex, oldPosition.ColumnIndex] = new Animal()
-                            {
-                                Type = AnimalType.Fish,
-                                Age = 0,
-                                Energy = 3482
-                            };
-                        }
-                    }
-                    else
-                    {
-                        // Shark move
-                        var newPositionCandidates = GetSharkSurroundingFieldCandidates(oldPosition, field);
-                        var newPosition = ChooseField(newPositionCandidates);
-                        var prey = field[newPosition.RowIndex, newPosition.ColumnIndex];
-
-                        if (animal.Energy == 0)
-                        {
-                            field[oldPosition.RowIndex, oldPosition.ColumnIndex] = null;
-                            continue;
-                        }
-
-                        if (prey is not null) animal.Energy += prey.Energy;
-
-                        animal.Age++;
-                        animal.Energy--;
-                        MoveToField(oldPosition, newPosition, field);
-                        moved.Add(newPosition);
-
-                        if (animal.Age % 4 == 0)
-                        {
-                            field[oldPosition.RowIndex, oldPosition.ColumnIndex] = new Animal()
-                            {
-                                Type = AnimalType.Shark,
-                                Age = 0,
-                                Energy = 3482
-                            };
-                        }
-                    }
+                    // Fish move
+                    var newPositionCandidates = GetFishSurroundingFieldCandidates(oldPosition, field);
+                    var newPosition = ChooseField(newPositionCandidates);
+                    animal.Age++;
+                    animal.Energy--;
+                    MoveToField(oldPosition, newPosition, field);
+                    moved.Add(newPosition);
+                    if (animal.Age % 3 == 0)
+                        SetAnimalAtPosition(new Animal {Type = AnimalType.Fish, Age = 0, Energy = 3482}, field,
+                            oldPosition);
                 }
+                else
+                {
+                    // Shark move
+                    var newPositionCandidates = GetSharkSurroundingFieldCandidates(oldPosition, field);
+                    var newPosition = ChooseField(newPositionCandidates);
+                    var prey = GetAnimalAtPosition(field, newPosition);
+
+                    if (animal.Energy == 0)
+                    {
+                        SetAnimalAtPosition(null, field, oldPosition);
+                        continue;
+                    }
+
+                    if (prey is not null) animal.Energy += prey.Energy;
+
+                    animal.Age++;
+                    animal.Energy--;
+                    MoveToField(oldPosition, newPosition, field);
+                    moved.Add(newPosition);
+
+                    if (animal.Age % 4 == 0)
+                        SetAnimalAtPosition(new Animal {Type = AnimalType.Shark, Age = 0, Energy = 3482}, field,
+                            oldPosition);
+                }
+            }
 
             return moved;
         }
@@ -86,11 +74,10 @@ namespace Wator.Core.Services
             throw new NotImplementedException();
         }
 
-        public static void MoveToField(Position oldPosition,
-            Position newPosition, Animal[,] field)
+        public static void MoveToField(Position oldPosition, Position newPosition, Animal[,] field)
         {
-            field[newPosition.RowIndex, newPosition.ColumnIndex] = field[oldPosition.RowIndex, oldPosition.ColumnIndex];
-            field[oldPosition.RowIndex, oldPosition.ColumnIndex] = null;
+            SetAnimalAtPosition(GetAnimalAtPosition(field, oldPosition), field, newPosition);
+            SetAnimalAtPosition(null, field, oldPosition);
         }
 
         public static Position ChooseField(IEnumerable<Position> candidates)
@@ -101,10 +88,12 @@ namespace Wator.Core.Services
         public static IEnumerable<Position> GetSurroundingFields<T>(
             Position position, T[,] field)
         {
-            yield return new Position((position.RowIndex - 1 + field.GetLength(0)) % field.GetLength(0), position.ColumnIndex);
+            yield return new Position((position.RowIndex - 1 + field.GetLength(0)) % field.GetLength(0),
+                position.ColumnIndex);
             yield return new Position(position.RowIndex, (position.ColumnIndex + 1) % field.GetLength(1));
             yield return new Position((position.RowIndex + 1) % field.GetLength(0), position.ColumnIndex);
-            yield return new Position(position.RowIndex, (position.ColumnIndex - 1 + field.GetLength(1)) % field.GetLength(1));
+            yield return new Position(position.RowIndex,
+                (position.ColumnIndex - 1 + field.GetLength(1)) % field.GetLength(1));
         }
 
         public static IEnumerable<Position> GetSharkSurroundingFieldCandidates(
@@ -127,17 +116,22 @@ namespace Wator.Core.Services
 
         private static bool ContainsFish(Position position, Animal[,] field)
         {
-            return field[position.RowIndex, position.ColumnIndex]?.Type == AnimalType.Fish;
+            return GetAnimalAtPosition(field, position)?.Type == AnimalType.Fish;
         }
 
         private static bool IsEmpty(Position position, Animal[,] field)
         {
-            return field[position.RowIndex, position.ColumnIndex] is null;
+            return GetAnimalAtPosition(field, position) is null;
         }
 
         private static Animal GetAnimalAtPosition(Animal[,] field, Position position)
         {
             return field[position.RowIndex, position.ColumnIndex];
+        }
+
+        private static void SetAnimalAtPosition(Animal animal, Animal[,] field, Position position)
+        {
+            field[position.RowIndex, position.ColumnIndex] = animal;
         }
     }
 }
