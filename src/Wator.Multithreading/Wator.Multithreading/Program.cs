d99ba2658fb62b12c66d04266641cd348b5bcb99
PaulAbloescher <abloescherpaul@gmail.com>
2021-05-15 19:24:47 +0200

    Use field builder.

--- src/Wator.Multithreading/Wator.Multithreading/Program.cs
+++ src/Wator.Multithreading/Wator.Multithreading/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Wator.Core.Entities;
+using Wator.Core.Helpers;
+using Wator.Core.Services;
 
 namespace Wator.Multithreading
 {
     public class Program
     {
-        static void Main(string[] args)
+        public const int FishCount = 15;
+        public const int SharkCount = 15;
+        public const int Rows = 10;
+        public const int Columns = 10;
+
+        public const int ThreadCount = 1;
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var simulation = new WatorSimulation(CreateField());
+            var splitIndices = FieldHelper.GetSplitIndices(simulation.Field, ThreadCount);
+
+            Parallel.For(0, ThreadCount, i =>
+            {
+                var workerIndices = splitIndices[i];
+                var innerTopBorder = workerIndices.FromRow + 1;
+                var outerTopBorder = workerIndices.ToRow - 1;
+                simulation.RunCycleInRows(innerTopBorder, outerTopBorder);
+            });
+        }
+
+        public static Animal[,] CreateField()
+        {
+            return new FieldBuilder()
+                .WithDimensions(Rows, Columns)
+                .WithFishCount(FishCount)
+                .WithSharkCount(SharkCount)
+                .Build();
         }
     }
 }
