8c7b9490a7e39e78a958d30fb602d138e84b2131
PaulAbloescher <abloescherpaul@gmail.com>
2021-05-15 17:59:58 +0200

    Create multithreading project.

---
+++ src/Wator.Multithreading/Wator.Multithreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wator.Multithreading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
