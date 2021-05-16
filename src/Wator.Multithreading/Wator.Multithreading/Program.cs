608e506868fc1dd4448bda3de9f0b920dccdce78
PaulAbloescher <abloescherpaul@gmail.com>
2021-05-16 12:21:36 +0200

    Ignore animals that moved to the border.

--- src/Wator.Multithreading/Wator.Multithreading/Program.cs
+++ src/Wator.Multithreading/Wator.Multithreading/Program.cs
@@ -10,8 +10,8 @@ namespace Wator.Multithreading
 {
     public class Program
     {
-        public const int FishCount = 10_000;
-        public const int SharkCount = 10_000;
+        public const int FishCount = 100_000;
+        public const int SharkCount = 100_000;
 
         public const int Rows = 10_000;
         public const int Columns = 10_000;
@@ -26,8 +26,9 @@ namespace Wator.Multithreading
             var splitBoundaries = FieldHelper.GetSplitBoundaries(simulation.Field, ThreadCount);
 
             var runtimeInMs = MeasureRuntime(() => ProcessIterations(simulation, splitBoundaries));
-
+            var averageRuntimeInMs = runtimeInMs / Iterations;
             Console.WriteLine($"Running {Iterations} iterations took {runtimeInMs}ms.");
+            Console.WriteLine($"Each iteration took {averageRuntimeInMs}ms on average.");
             Console.WriteLine("Press any key to quit.");
             Console.ReadKey(false);
         }
@@ -48,7 +49,7 @@ namespace Wator.Multithreading
 
         public static void ProcessIteration(WatorSimulation simulation, (int, int)[] splitBoundaries)
         {
-            var ignoreInBorderProcessing = new IEnumerable<Position>[splitBoundaries.Length];
+            var ignoreInBorderProcessing = new HashSet<Position>[splitBoundaries.Length];
             Parallel.For(0, splitBoundaries.Length, i =>
             {
                 var (fromRow, toRow) = splitBoundaries[i];
@@ -57,16 +58,20 @@ namespace Wator.Multithreading
                 ignoreInBorderProcessing[i] = simulation.RunCycleInRows(innerTopBorder, outerTopBorder);
             });
 
-            foreach (var (fromRow, toRow) in splitBoundaries)
+            for (var i = 0; i < splitBoundaries.Length; i++)
             {
-                simulation.RunCycleInRows(fromRow, fromRow);
-                simulation.RunCycleInRows(toRow, toRow);
+                var(fromRow, toRow) = splitBoundaries[i];
+                var ignorePositions = ignoreInBorderProcessing[i];
+                simulation.RunCycleInRows(fromRow, fromRow, ignorePositions);
+                simulation.RunCycleInRows(toRow, toRow, ignorePositions);
             }
         }
 
         public static Animal[,] CreateField()
         {
             return new FieldBuilder()
+                .WithConfiguration(new WatorConfiguration{ FishBreedTime = 1, Seed = 42})
+                .WithSeed(101)
                 .WithDimensions(Rows, Columns)
                 .WithFishCount(FishCount)
                 .WithSharkCount(SharkCount)
